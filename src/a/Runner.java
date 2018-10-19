afa5201a718d08e3747afecefa2fc0d85a3d1f5d
BT_1N3_13@BKTECH.LOCAL <BT_1N3_13@BKTECH.LOCAL>
2018-10-19 08:36:28 -0400

    Initial commit

---
+++ src/a/Runner.java
@@ -0,0 +1,10 @@
+package a;
+
+public class Runner {
+
+    public static void main(String[] args) {
+	// declare and initalize a teacher, an array of students, and a classroom
+        // print class and print class average
+        // randomStudent() builds a random student with a random first name, last name, and GPA
+    }
+}
