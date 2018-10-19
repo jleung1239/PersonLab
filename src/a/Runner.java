cedf5e75b7524b285247f259f9423b5afdda5cec
jleung1239 <jleung1239>
2018-10-19 08:43:02 -0400

    a

--- src/a/Runner.java
+++ src/a/Runner.java
@@ -3,6 +3,8 @@ package a;
 public class Runner {
 
     public static void main(String[] args) {
+        static String[] firstNames; // premade list of first names
+        static String[] familyNames; //premade list of family names
 	// declare and initalize a teacher, an array of students, and a classroom
         // print class and print class average
         // randomStudent() builds a random student with a random first name, last name, and GPA
