2ecb26d1102800fcba7a987fe9b00f3edac5110d
BT_1N3_13 <a>
2018-10-25 08:42:50 -0400

    a

--- src/a/Teacher.java
+++ src/a/Teacher.java
@@ -5,7 +5,20 @@ public class Teacher extends Person{
     private String title;
     public Teacher(String subject, String title)
     {
+        super("Teacher");
         this.subject = subject;
         this.title = title;
+    }
+
+    @Override
+    public String getSubject()
+    {
+        return subject;
+    }
+
+    @Override
+    public String toString()
+    {
+        return title +"."+ getFamilyName();
     }
 }
